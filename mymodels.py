e658c19471c8527491a1f391d6a706343b630d41
Tomé Albuquerque <56262226+tomealbuquerque@users.noreply.github.com>
2022-10-20 16:15:39 +0200

    Update mymodels.py

--- mymodels.py
+++ mymodels.py
@@ -48,3 +48,40 @@ class Base(nn.Module):
         return Phat.argmax(1)
 
 
+class MultiMRI(nn.Module):
+    def __init__(self, pretrained_model, n_outputs=4):
+        super().__init__()
+        self.n_outputs = n_outputs
+        model = getattr(models, pretrained_model)(pretrained=True)
+        model.conv1 = nn.Conv2d(155, 64, kernel_size=7, stride=2, padding=3,bias=False)
+        model = nn.Sequential(*tuple(model.children())[:-1])
+        last_dimension = torch.flatten(model(torch.randn(1, 155, 240, 240))).shape[0]
+        self.model = nn.Sequential(
+            model,
+            nn.Flatten(),
+            nn.Dropout(0.2),
+            nn.Linear(last_dimension, 512),
+            nn.Dropout(0.2),
+            nn.ReLU())
+            
+
+        self.sharedlayers = nn.Sequential(
+            nn.Linear(512*4, 512),
+            nn.ReLU(),
+            nn.Linear(256, n_outputs)
+        )
+
+    def forward(self, x):
+        Enc = [self.model(X) for X in x]
+        return self.sharedlayers(torch.stack(Enc))
+
+    def loss(self, Yhat, Y):
+        return ce(Yhat, Y)
+
+    def to_proba(self, Yhat):
+        return F.softmax(Yhat, 1)
+
+    def to_classes(self, Phat):
+        return Phat.argmax(1)
+
+

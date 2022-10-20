2c4d435d4571817db147db646d3cafa64a5a4c42
tomealbuquerque <tome.m.albuquerque@inesctec.pt>
2022-10-20 15:34:47 +0100

    updates

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--architecture', choices=['inception_v3', 'mnasnet1_0', 'mobilenet_v2', 'resnet18',
     'resnext50_32x4d', 'vgg16','wide_resnet50_2'],default='resnext50_32x4d')
 parser.add_argument('--method', choices=['Base'], default='Base')
+parser.add_argument('--MRI_type', choices=['flair'], default='flair')
 parser.add_argument('--fold', type=int, choices=range(10), default=0)
 parser.add_argument('--epochs', type=int, default=50)
 parser.add_argument('--batchsize', type=int, default=32)
@@ -29,9 +30,9 @@ import mydataset, mymodels
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-tr_ds = mydataset.MyDataset_MRI('train', mydataset.train_transforms, args.fold)
+tr_ds = mydataset.MyDataset_MRI_all('train', mydataset.train_transforms, args.fold)
 tr = DataLoader(tr_ds, args.batchsize, True, pin_memory=True)
-ts_ds = mydataset.MyDataset_MRI('test', mydataset.val_transforms, args.fold)
+ts_ds = mydataset.MyDataset_MRI_all('test', mydataset.val_transforms, args.fold)
 ts = DataLoader(ts_ds, args.batchsize, pin_memory=True)
 
 def test(val):
@@ -39,6 +40,8 @@ def test(val):
     val_avg_acc = 0
     for XX, Y in tqdm(val):
         X = [X.to(device, torch.float) for X in XX]
+        if args.MRI_type == 'flair':
+            X=X[0]
         Y = Y.to(device, torch.int64)
         Yhat = model(X)
         Khat = model.to_classes(model.to_proba(Yhat))
@@ -55,6 +58,8 @@ def train(tr, val, epochs=args.epochs, verbose=True):
         avg_loss = 0
         for XX, Y in tqdm(tr):
             X = [X.to(device, torch.float) for X in XX]
+            if args.MRI_type == 'flair':
+                X=X[0]
             Y = Y.to(device, torch.int64)
             opt.zero_grad()
             Yhat = model(X)
@@ -78,6 +83,8 @@ def predict_proba(data):
     Phat = []
     with torch.no_grad():
         for X, _ in data:
+            if args.MRI_type == 'flair':
+                X=X[0]
             phat = model.to_proba(model(X.to(device, dtype=torch.float)))
             Phat += list(phat.cpu().numpy())
     return Phat
